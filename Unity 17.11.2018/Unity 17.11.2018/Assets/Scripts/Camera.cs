9411adf0d322b825b8688fc7d26c6b20edf51d58
vitaliy <vitaliy.net>
2018-11-17 20:33:21 +0300

    My first project

---
+++ Unity 17.11.2018/Unity 17.11.2018/Assets/Scripts/Camera.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+   
+public class Camera : MonoBehaviour {
+    public Transform cube;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3 (cube.position.x, cube.position.y, transform.position.z);
+	}
+}
---
+++ Unity 17.11.2018/Unity 17.11.2018/Assets/Scripts/CubeMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMovement : MonoBehaviour {
+    public float force = 5.0f;
+    public float speed = 5.0f;
+	// Use this for initialization
+	void Start () {
+        force = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            gameObject.GetComponent<Rigidbody>().AddForce(transform.up * force);
+            force = 5;
+            gameObject.transform.Translate(Vector3.right * Time.deltaTime * speed);
+        }
+        else
+        {
+
+            gameObject.GetComponent<Rigidbody>().AddForce(transform.up * -force);
+            gameObject.transform.Translate(Vector3.right * Time.deltaTime * speed);
+        }
+	}
+}
